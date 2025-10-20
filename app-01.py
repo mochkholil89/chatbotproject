fb1d640be916cefbcc9e9d7345ec5b82dfe47c37
mochkholil89 <moch.kholil89@gmail.com>
2025-10-20 22:08:12 +0700

    chatbot commit

--- app-01.py
+++ app-01.py
@@ -16,7 +16,6 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 messages = [
     SystemMessage(content="You are a funny assistant that always joking.")
 ]
-coba
 
 while True:
     prompt = input("User: ")
